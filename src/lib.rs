6551e10e5bc958a071d86d18078150ded617a83a
Martin Benes <martinbenes1996@gmail.com>
2025-05-01 22:19:28 +0200

    I broke my rust

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ mod spam;
 #[pymodule]
 fn _sealwatch(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
 	let spam_mod = PyModule::new_bound(py, "spam_rs")?;
-    spam::spam(py, &spam_mod)?;
+    spam::spam_rs(py, &spam_mod)?;
     m.add_submodule(&spam_mod)?;
 
     // m.add_function(wrap_pyfunction!(extract, m)?)?;
